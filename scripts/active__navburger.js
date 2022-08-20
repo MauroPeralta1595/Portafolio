05cc8bec768b582fa5ca774dd4e01bdb58c7d949
Mauro Peralta <mauroperalta1595@gmail.com>
2022-08-19 21:08:25 -0300

    Corregido bug de hamburger button y algunos detalles

--- scripts/active__navburger.js
+++ scripts/active__navburger.js
@@ -1,8 +1,12 @@
+const HTML__SELECTOR = document.querySelector('html');
+const PAGE__SELECTOR = document.querySelector('.page');
 const NAV__HAMBURGER = document.querySelector('.nav__hamburger');
 const NAV__MENU__SELECTOR = document.querySelector('.nav__menu');
 const LINK__NAV__MENU = document.querySelectorAll('.nav__link');
 
 function SHOW__NAV__MENU() {
+    HTML__SELECTOR.classList.toggle('active__navburger');
+    PAGE__SELECTOR.classList.toggle('active__navburger');
     NAV__HAMBURGER.classList.toggle('active__navburger');
     NAV__MENU__SELECTOR.classList.toggle('active__navburger');
 }
