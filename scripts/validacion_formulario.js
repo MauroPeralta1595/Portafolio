8ede36132fcd59dee5b4d3d9c0a0010745958ce1
Mauro Peralta <mauroperalta1595@gmail.com>
2022-12-05 21:22:15 -0300

    Corrección de errores con formulario, actualización de datos.

--- scripts/validacion_formulario.js
+++ scripts/validacion_formulario.js
@@ -1,5 +1,5 @@
 $(function() {
-    const emailVálido = email => {
+    const emailValido = email => {
     return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
     }
     $('.error').hide();
@@ -21,7 +21,7 @@ $(function() {
             $("label#email_error").show();
             $("input#email").focus();
             return false;
-        }else if (!emailVálido(email)) {
+        }else if (!emailValido(email)) {
             $("label#email_error").text("Por favor, escribe un correo electrónico válido");
             $("label#email_error").show()
             $("input#email").focus();
@@ -45,29 +45,35 @@ $(function() {
         }
 
         var dataString = 'name='+ name + '&email=' + email + '&subject=' + subject + '&textarea=' + textarea;
+
         $.ajax({
-            method: "POST",
+            method: "POSTE",
             url: "https://formsubmit.co/ajax/676b1aa032ca54d23e4e5e871085ee7f",
             dataType: "json",
             data: dataString,
             accepts: 'application/json',
+            success: () => {
+                $('#contact__form').html("<div id='message'></div>");
+                $('#message').html("<img id='checkmark' src='assets/check.svg' class='message__check' />")
+                .append("<h2 class='message__title'> Gracias por comunicarse</h2>")
+                .append("<h3 class='message__subtitle'> Su mensaje ha sido enviado correctamente.</h3>")
+                .append("<p class='message__text'> Me pondré en contacto con usted a la brevedad </p>")
+                .hide()
+                .fadeIn(700);
+            },
+            error: () =>{
+                $('#contact__form').html("<div id='message--error'></div>");
+                $('#message--error').html("<img id='checkmark' src='assets/failure.svg' class='message__check' />")
+                .append("<h2 class='message__title'> ¡Error! </h2>")
+                .append("<p class='message__text--error'> Ha ocurrido un error, por favor vuelva a intentarlo nuevamente.</p>")
+                .hide()
+                .fadeIn(700);
+            }
         });
-
-        function showMessage(){
-            $('#contact__form').html("<div id='message'></div>");
-            $('#message').html("<img id='checkmark' src='assets/check.svg' class='message__check' />")
-            .append("<h2 class='message__title'> Gracias por comunicarse</h2>")
-            .append("<h3 class='message__subtitle'> Su mensaje ha sido enviado correctamente.</h3>")
-            .append("<p class='message__text'> Me pondré en contacto con usted a la brevedad </p>")
-            .hide()
-            .fadeIn(700);
-        }
-        
-        
-        showMessage();
        
         return false;
     });
 });
+
 
 
